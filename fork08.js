d51fc875b176ea3fbb5e7d3e4fc091703448762c
NekoShelly <michelledandrea2008@gmail.com>
2026-04-23 21:37:44 +0200

    Fix inverti_cifre to reverse digits of a number
    Refactor inverti_cifre function to correctly reverse digits.

--- fork08.js
+++ fork08.js
@@ -1,9 +1,11 @@
+fork08.js
 let n=359;
-let inverti_cifre={
-	risultato:
-	let r;
-	for(let i=0;i<n.length();i++){
-		r+=n.charAt(i);
-	}
-	return r;
-};
+function inverti_cifre(num) {
+    let s=num.toString(); //converto n da numero a stringa
+    let r="";
+    for (let i=s.length-1;i>=0;i--) { //uso il for decrementando i per invertire le cifre di s
+        r+=s.charAt(i);
+    }
+    return r; //contiene le cire di n al contrario
+}
+console.log(inverti_cifre(n)); //richiamo la funzione
