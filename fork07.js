0f7102e1952e7875f98c794101f62ef131cd32e3
NekoShelly <michelledandrea2008@gmail.com>
2026-04-26 19:42:25 +0200

    Add 'Genere' property to book object

--- fork07.js
+++ fork07.js
@@ -1,4 +1,4 @@
-let book={Titolo:"Imperfetti", Autore:"Luigi Ballerini", Pubblicazione:2016, Pagine:336};
+let book={Titolo:"Imperfetti", Autore:"Luigi Ballerini", Pubblicazione:2016, Genere:"romanzo distopico", Pagine:336};
 // Stampa le proprieta' dell'oggetto sulla console
 for (let key in book) {
   //stampiamo nome della chiave e valore associato
